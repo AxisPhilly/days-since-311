d3593b651627061938ada712f07380f65f722d85
pselle <pamela.selle@gmail.com>
2013-06-24 17:17:48 -0400

    Some starter we might want to use for creating counters

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-if (typeof app === 'undefined' || !app) {
-  var app = {};
+var app = app || {};
+
+// Load JSON into a readable variable.
+
+function SinceCounter() {
+  this.events = [];
+  this.findEvents = function() {
+    //parse JSON and populate the events array
+    return false;
+  };
+  this.count = function() {
+      var currentTime = new Date();
+      return currentTime - events[0]['time'];
+  };
 }
+
+var potholeCounter = new SinceCounter({terms: 'pothole'});
