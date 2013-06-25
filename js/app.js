2c32e1fbd46f45503660384beb12d43bc6b0b155
pselle <pamela.selle@gmail.com>
2013-06-25 15:34:36 -0400

    Sepearate out into other functions, add render method

--- js/app.js
+++ js/app.js
@@ -15,24 +15,48 @@ var sinceCounter = {
   }
 };
 
+app.searchData = function(data, terms){
+  return ['testing'];
+};
+
+app.loadData = function(data, callback) {
+    // I refuse to use jQuery just for AJAX!
+  var r = new XMLHttpRequest();
+  r.open("GET", data, true);
+  r.onreadystatechange = function () {
+    if (r.readyState != 4 || r.status != 200) return;
+    console.log(JSON.parse(r.responseText));
+    // populate the counters with their data
+    var data = JSON.parse(r.responseText);
+    for(var counter in app.counters) {
+      if(app.counters.hasOwnProperty(counter)){
+        // Search the data for entries matching the terms
+        var entries = app.searchData(data, counter.terms);
+        app.counters[counter].entries = entries;
+        console.log(app.counters[counter]);
+        console.log(entries);
+      }
+    }
+    callback();
+  };
+  r.send({});
+};
+
+app.render = function() {
+  // Render the counters on the page, loading templates, etc.
+  console.log(app.counters.potholeCounter.entries);
+};
+
+
 /*
   Initialization function for the app.
   To create a new counter, instantiate it here.
 */
 app.init = function() {
-  var currentData;
-
-  // I refuse to use jQuery just for AJAX!
-  var r = new XMLHttpRequest();
-  r.open("GET", '/311data.json', true);
-  r.onreadystatechange = function () {
-    if (r.readyState != 4 || r.status != 200) return;
-    console.log(JSON.parse(r.responseText));
-  };
-  r.send({});
-
   // Instantiate counters
-  var potholeCounter = Object.create(sinceCounter, { terms: ['pothole'] });
+  app.counters = {};
+  app.counters.potholeCounter = Object.create(sinceCounter, { terms: ['pothole'] });
+  app.loadData('/311data.json', function(){ app.render(); });
 };
 
 app.init();
