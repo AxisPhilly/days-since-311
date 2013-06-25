46a653d78001cbaee8539e30eca3a689c008e123
pselle <pamela.selle@gmail.com>
2013-06-25 13:26:49 -0400

    Use the 311 library to grab data

--- scripts/data.js
+++ scripts/data.js
@@ -1,7 +1,24 @@
+var fs = require('fs');
 var phl311 = require('./phl311');
 
-// load the current JSON file
+var currentData = '../data/311data.json';
 
-// request data from 311
+phl311.requests({}, function(resp) {
+    var parsed = JSON.stringify(resp);
+    fs.writeFile('311data.json', parsed, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+    });
 
-// If success, and date on 311 data is newer than JSON, add new JSON file
+    // Use this snip to get dates out of date_created
+    // var last_req = resp.requests[0];
+    // console.log(last_req.date_created);
+    // var date = new Date(last_req.date_created*1000);
+    // var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    // console.log(months[date.getMonth()] + ' ' + date.getDate());
+
+    // If success, and date on 311 data is newer than JSON, add new JSON file
+});
